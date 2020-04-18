e001744fb9a51bfdec68180d13a400313b8833cb
cloutmuppet <61170459+cloutmuppet@users.noreply.github.com>
2020-04-18 18:35:26 -0500

    added elixir

---
+++ elixir/bot.ex
@@ -0,0 +1,45 @@
+defmodule ExampleSupervisor do
+  def deps do
+    [{:nostrum, "~> 0.4"}]
+  end
+  def start do
+    import Supervisor.Spec
+
+    # List comprehension creates a consumer per cpu core
+    children = for i <- 1..System.schedulers_online, do: worker(ExampleConsumer, [], id: i)
+
+    Supervisor.start_link(children, strategy: :one_for_one)
+  end
+end
+
+defmodule ExampleConsumer do
+
+  use Nostrum.Consumer
+
+  alias Nostrum.Api
+
+  require Logger
+
+  def start_link do
+    Consumer.start_link(__MODULE__)
+  end
+
+  def handle_event({:MESSAGE_CREATE, {msg}, ws_state}, state) do
+    case msg.content do
+      # In general, you don't want to match using the binary notation, but I'm
+      # doing it here to be explicit
+      <<"!" :: binary, "ping" :: binary>> ->
+        Api.create_message(msg.channel.id, "I copy and pasted this code")
+      _ ->
+        :ignore
+    end
+
+    {:ok, state}
+  end
+
+  # Default event handler, if you don't include this, your consumer WILL crash if
+  # you don't have a method definition for each event type.
+  def handle_event(_, state) do
+    {:ok, state}
+  end
+end
---
+++ elixir/config/config.exs
@@ -0,0 +1,5 @@
+config :nostrum,
+  token: "Njc5NDU0Njg0NjI0MzIyNTg3.XpnGKw.4WFcPzWOSFCXkzMsU0ixsBc3VBU", # The token of your bot as a string
+  num_shards: :auto # The number of shards you want to run your bot under, or :auto.
+config :logger,
+  level: :warn
