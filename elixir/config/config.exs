d2a28e477fea7771ec5574f0cff87b3121ff4a8f
cloutmuppet <61170459+cloutmuppet@users.noreply.github.com>
2020-04-18 18:37:05 -0500

    Update config.exs

--- elixir/config/config.exs
+++ elixir/config/config.exs
@@ -1,5 +1,5 @@
 config :nostrum,
-  token: "Njc5NDU0Njg0NjI0MzIyNTg3.XpnGKw.4WFcPzWOSFCXkzMsU0ixsBc3VBU", # The token of your bot as a string
+  token: "BOT-TOKEN", # The token of your bot as a string
   num_shards: :auto # The number of shards you want to run your bot under, or :auto.
 config :logger,
   level: :warn
